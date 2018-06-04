61ae7613b78f1b16e0743946171d3f586a0a8a90
newzhoujian <newzhoujian@gmail.com>
2018-06-04 19:14:16 +0800

    add PreProcess

--- src/PreProcess.py
+++ src/PreProcess.py
@@ -2,18 +2,30 @@
 # -*- coding:utf-8 -*-
 
 import pandas as pd
+import tensorflow as tf
 from itertools import chain
 import sys
 import numpy as np
 import jieba
 import keras
-from keras.layers import Dense, Embedding, LSTM, TimeDistributed, Input, Bidirectional, GRU
+from collections import defaultdict
+from gensim.models.word2vec import Word2Vec
+import gensim
+from keras.layers import Dense, Embedding, LSTM, TimeDistributed, Input, Bidirectional, GRU, recurrent
 from keras.models import Model
-
+# f = pd.read_excel('../data/temp.xlsx', header=0)
 f = pd.read_excel('../data/temp.xlsx', header=0)
 # f = pd.read_csv('data/data.csv', header=None, sep=',')
 # f = open('data/smartPatent_20180512.xlsx', 'r')
+stopwordfile = open('../data/StopWords_con.txt', 'r')
 
+def getstopwordset():
+    w = set()
+    for lines in stopwordfile:
+        arr = lines.split('\n')
+        # print type(arr[0].decode('utf-8'))
+        w.add(arr[0].decode('utf-8'))
+    return w
 
 def cut_title(str):
     seg_list = jieba.cut(str)
@@ -102,31 +114,64 @@ def get_str_X():
     f[u'标题'] = f[u'标题'].apply(cut_title)
     f[u'摘要'] = f[u'摘要'].apply(cut_abstract)
     f[u'首项权利要求'] = f[u'首项权利要求'].apply(cut_abstract)
-
+    print 'cut word down!'
+    print 'get stop word!'
+    stopwordset = getstopwordset()
+    print 'get down!'
     tempX = []
     X = []
-    for i in range(1, len(f[u'标题'])):
-        tempX.append(f[u'标题'][i])
+    # sys.exit()
+    for i in range(1, len(f[u'标题'])+1):
+        # tempX.append(f[u'标题'][i])
+        for j in f[u'标题'][i]:
+            isin = (j in stopwordset)
+            if not isin:
+                tempX.append(j)
         for j in f[u'摘要'][i]:
-            ed = len(j)
-            for k in range(len(j)):
-                if j[k] == u'<':
-                    ed = k
-                    break
-            tempX.append(j[:ed])
+            for k in j:
+                isin = (k in stopwordset)
+                if not isin:
+                    tempX.append(k)
 
         for j in f[u'首项权利要求'][i]:
-            ed = len(j)
-            for k in range(len(j)):
-                if j[k] == u'<':
-                    ed = k
-                    break
-            tempX.append(j[:ed])
+            for k in j:
+                isin = (k in stopwordset)
+                if not isin:
+                    tempX.append(k)
+        print tempX
         X.append(tempX)
         tempX = []
+
     return X
 
 
+def get_embedding_X():
+    X = get_str_X()
+    #sys.exit()
+    modelword2vec = Word2Vec.load('../word2vec/word2vec.model')
+    sess_X = []
+    sent_X = []
+    fin_X = []
+    for i in X:
+        for j in i:
+            for k in j:
+                # print chars2ids[k]
+                tempword = gensim.utils.to_unicode(k)
+                if tempword in modelword2vec:
+                    sent_X.append(np.array([w for w in modelword2vec[tempword]]))
+                else:
+                    sent_X.append(np.array([0.] * modelword2vec.vector_size))
+            sent_X = np.array(sent_X)
+            sess_X.append(sent_X)
+            sent_X = []
+        sess_X = np.array(sess_X)
+        fin_X.append(sess_X)
+        sess_X = []
+
+    print 'embedding down!'
+    return fin_X, modelword2vec.vector_size
+
+'''
 def get_X():
     X = get_str_X()
     chars = set()
@@ -150,12 +195,14 @@ def get_X():
                 X[i][j][k] = chars2ids[X[i][j][k]]
 
     return X, cnt
+'''
+
 
 
 def get_y():
     tags = set()
     for i in f[u'主IPC分类号-小类'].values:
-        tags.add(i)
+        tags.add(i[0])
 
     tags2ids = {}
 
@@ -164,21 +211,41 @@ def get_y():
         tags2ids[i] = cnt
         cnt += 1
 
-    f[u'主IPC分类号-小类'] = f[u'主IPC分类号-小类'].apply(lambda str_y: tags2ids[str_y])
+    f[u'主IPC分类号-小类'] = f[u'主IPC分类号-小类'].apply(lambda str_y: tags2ids[str_y[0]])
     y = f[u'主IPC分类号-小类'].values
 
-    return y
+    ids2onehot = {}
+    j = 0
+    for i in tags:
+        temp = [0] * len(tags)
+        temp[j] = 1
+        ids2onehot[tags2ids[i]] = temp
+        j += 1
+
+    y_onehot = []
+    for i in range(len(y)):
+        y_onehot.append(ids2onehot[y[i]])
+
+    print 'get y!'
+    return y_onehot, tags2ids
 
 
 def get_padding_X(sentmaxlen, sessmaxlen):
-    X, cnt = get_X()
+    X, vecsize = get_embedding_X()
     for i in range(len(X)):
         for j in range(len(X[i])):
             if len(X[i][j]) >= sentmaxlen:
                 X[i][j] = X[i][j][:sentmaxlen]
             else:
-                temp = [0] * (sentmaxlen - len(X[i][j]))
-                X[i][j] = temp + X[i][j]
+                temp = np.array([np.array([0.] * vecsize)] * (sentmaxlen - len(X[i][j])))
+                if len(X[i][j]) != 0:
+                    X[i][j] = np.concatenate((temp, X[i][j]), axis=0)
+
+                else:
+                    X[i][j] = temp
+
+
+
     '''
     if len(X) >= sessmaxlen:
         X = X[:sessmaxlen]
@@ -188,27 +255,109 @@ def get_padding_X(sentmaxlen, sessmaxlen):
     '''
     for i in range(len(X)):
         if len(X[i]) >= sessmaxlen:
-            X[i] = X[i][:sessmaxlen]
+            temp = np.array([X[i][0]])
+            for j in range(1, 40):
+                temp = np.concatenate((temp, [X[i][j]]), axis=0)
+            X[i] = temp
         else:
-            temp = [[0] * sentmaxlen] * (sessmaxlen - len(X[i]))
-            X[i] = temp + X[i]
-    return X
+            temp = np.array([[np.array([0.] * vecsize)] * sentmaxlen] * (sessmaxlen - len(X[i])))
+            for j in X[i]:
+                temp = np.concatenate((temp, [j]), axis=0)
+            X[i] = temp
+
+    print 'padding down!'
+    return X, vecsize
 
 
 sent_maxlen = 32
 sess_maxlen = 40
 word_size = 200
+sent_size = 200
+sess_size = 200
+batch_size = 1000
 
-X, cnt = get_padding_X(sentmaxlen=sent_maxlen, sessmaxlen=sess_maxlen)
-y = get_y()
+# X, vecsize = get_embedding_X()
 
 
-sequence = Input(shape=(sent_maxlen,), dtype='int32')
-embedded = Embedding(cnt, word_size, input_length=sent_maxlen, mask_zero=True)(sequence)
-sen2vec = GRU(200, return_sequences=True)(embedded)
-output = TimeDistributed(Dense(1))(sen2vec)
-model = Model(input=sequence, output=output)
+X, vecsize = get_padding_X(sentmaxlen=sent_maxlen, sessmaxlen=sess_maxlen)
+y, tags2ids = get_y()
+
+n_class = len(tags2ids)
+
+y = np.array(y)
+
+X_word2vec = np.array([i for i in X])
+X_word2vec = np.reshape(X_word2vec, (len(X), -1, vecsize))
+print 'data and label convert down!'
+# rint type(X_word2vec)
+
+# print type(y[0])
+
+# print X_word2vec.shape
+# print y.shape
+#print X[0]
+#print X_word2vec[:40]
+# print y
+
+print 'begin training...'
+model_input = Input(shape=(sent_maxlen*sess_maxlen, vecsize), dtype=tf.float32)
+sen2vec = GRU(sent_size, activation='tanh', return_sequences=True)(model_input)
+sess2vec = GRU(sess_size, return_sequences=False)(sen2vec)
+model_output = Dense(len(tags2ids), activation='softmax')(sess2vec)
+# model_output = Dense(1)(sess2vec)
+model = Model(inputs=model_input, outputs=model_output)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(X_word2vec, y, batch_size=batch_size, epochs=50)
+print 'end training!'
+print 'save model!'
+model.save('PatentClassification.h5')
+print 'save model down!'
+
+
+
+'''
+sent = []
+sess = []
+sent2vec = Input(shape=(sent_maxlen,), dtype='int32')  # (32,)
+print type(sent2vec)
+for i in range(sess_maxlen):
+    sent.append(sent2vec)  # 40 * (32, )
+for word2vecs in sent:
+    embedded = Embedding(cnt, word_size, input_length=sent_maxlen, mask_zero=True)(word2vecs)  # (32, 200)
+    sen2vec = GRU(sent_size, activation='tanh', return_sequences=True)(embedded)  # (32, 200)
+    sess.append(sen2vec)
+
+sess = tf.convert_to_tensor(sess)  # (40, 32, 200)
+sess = tf.reshape(sess, (sess_maxlen, sent_maxlen, -1))
+sess2vec = GRU(sess_size, return_sequences=False)(sess)
+# sess2vec = tf.reshape(sess2vec, (-1, ))
+# output = TimeDistributed(Dense(5, activation='softmax'))(sess2vec)
+# output = Dense(len(tags2ids), activation='softmax')(sess2vec)
+output = Dense(5, activation='softmax')(sess2vec)
+model = Model(inputs=sent2vec, outputs=output)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(X_word2vec, y, batch_size=batch_size, epochs=50)
+
+
+
+sent = []
+sess = []
+sent2vec = Input(shape=(sent_maxlen, vecsize), dtype=tf.float32)
+for i in range(sess_maxlen):
+    sent.append(sent2vec)
+for sent in sent:
+    sent2vec_res = GRU(sent_size, activation='tanh', return_sequences=True)(sent)  # (32, 200)
+    sess.append(sent2vec_res)
+
+sess = tf.convert_to_tensor(sess)
+sess = tf.reshape(sess, (sess_maxlen, sent_size, -1))
+sess2vec = GRU(sess_size, return_sequences=False)(sess)
+output = TimeDistributed(Dense(5, activation='softmax'))(sess2vec)
+model = Model(inputs=sent2vec, outputs=output)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(X_word2vec, y, batch_size=batch_size, epochs=50)
+'''
+
 
 '''
 all_tags = list(chain(*f[u'主IPC分类号-小类'].values))
